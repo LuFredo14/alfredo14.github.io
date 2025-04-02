0540a685f8676370e99f25333312c4726a3f1a02
LuFredo14 <47934866@continental.edu.pe>
2025-04-02 17:52:28 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  sites: 'https://alfredo14.github.io',
   devToolbar: {
     enabled: false
   },
